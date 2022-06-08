6664d49106653d63cdc07eb568705c7e1540f904
LDHugeman <luisdavid.vp@gmail.com>
2022-06-08 18:55:18 +0200

    Clase 6

--- options/src/App.vue
+++ options/src/App.vue
@@ -1,19 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld />
+  <button @click="show = !show">Menu</button>
+  <transition name="fade">
+    <Menu v-show="show" />
+  </transition>
 </template>
 
 <script>
-import { defineAsyncComponent } from "vue";
-
-const HelloWorld = defineAsyncComponent(() =>
-  import("./components/HelloWorld.vue")
-);
-
+import Menu from "./components/Menu";
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  components: { Menu },
+  data() {
+    return {
+      show: false,
+    };
   },
 };
 </script>
@@ -26,5 +26,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.fade-enter-from {
+  opacity: 0;
+}
+.fade-leave-to {
+  opacity: 0;
+}
+.fade-leave-active,
+.fade-enter-active {
+  transition: opacity 0.5s ease;
 }
 </style>

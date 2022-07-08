0b439d02b4da4cfe1c03952d5ea212b0be37f066
LDHugeman <luisdavid.vp@gmail.com>
2022-07-08 19:14:46 +0200

    Clase 13

---
+++ test/src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>Hola</div>
+</template>
+
+<script>
+import { onMounted } from "vue";
+export default {
+  name: "HomeComponent",
+  setup() {
+    onMounted(()=>{
+      console.log("mounted!");
+    });
+  },
+};
+</script>

5c8daca5c1d17945f5f5da30f0d2a73831f82b2c
LDHugeman <luisdavid.vp@gmail.com>
2022-07-10 17:17:05 +0200

    Clase 17

--- test/src/components/Home.vue
+++ test/src/components/Home.vue
@@ -3,18 +3,20 @@
 </template>
 
 <script>
-import { ref, computed } from "vue" 
+import { toRefs, computed } from "vue" 
 export default {
+  props: {
+    firstName: String,
+    lastName: String,
+  },
   name: "HomeComponent",
-  setup() {
-    const firstName = ref("Diana");
-    const lastName = ref("Martinez");
+  
+  setup(props) {
+    const {firstName, lastName} = toRefs(props);
 
     const fullName = computed(()=>{
       return `${firstName.value} ${lastName.value}`;
     })
-
-    
 
     return{
       fullName
